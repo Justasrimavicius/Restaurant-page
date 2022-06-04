7e8ffdffb22d513cf803ecdb6931a85edb94c75d
Justinas <justasrimavicius@gmail.com>
2022-06-04 14:41:06 +0300

    Made the first page of the restaurant. Next: wrap the page in a seperate module(not inside of index.js), create 2 more modules(2 more pages) and assign all 3 modules(pages) to index.js

--- src/index.js
+++ src/index.js
@@ -1,2 +1,50 @@
-console.log('papa');
 import css from "./style.css";
+
+
+const content=document.getElementById('content');
+const heading=document.createElement('h1');
+heading.innerText='My amazing Restaurant';
+
+const headingSmall=document.createElement('h2');
+headingSmall.innerText='Our todays food looks like this:';
+
+const foodDiv=document.createElement('div');
+foodDiv.id='food-div';
+
+
+
+
+function createFood(){
+
+    const bowlText=document.createElement('p');
+    bowlText.innerText='This chia bowl is packed with fiber, low in sugar, and is the perfect breakfast to fuel you for the day!';
+    const bowlImg=document.createElement('img');
+    bowlImg.src='../src/bowl.jpg';
+    bowlImg.width=200;
+    const bowlDiv=document.createElement('div');
+    bowlDiv.append(bowlText,bowlImg);
+
+    const ketoText=document.createElement('p');
+    ketoText.innerText='This keto plate is particularly great for all of you low carb high fat lowers!';
+    const ketoImg=document.createElement('img');
+    ketoImg.src='../src/keto.jpg';
+    ketoImg.width=200;
+    const ketoDiv=document.createElement('div');
+    ketoDiv.append(ketoText,ketoImg);
+
+    const wrapText=document.createElement('p');
+    wrapText.innerText='You love that crispy feeling of a crunchy wrap in your mouth, dont you? Try out this wrap - you will love it even more!';
+    const wrapImg=document.createElement('img');
+    wrapImg.src='../src/wrap.jpg';
+    wrapImg.width=200;
+    const wrapDiv=document.createElement('div');
+    wrapDiv.append(wrapText,wrapImg);
+
+    foodDiv.append(bowlDiv,ketoDiv,wrapDiv);
+
+};
+createFood();
+
+content.append(heading,headingSmall,foodDiv);
+
+
